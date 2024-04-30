2b32e2755b01538cf352ce18811ea6112f40f86c
Davide Kato <davide.0.kato@gmail.com>
2024-04-30 17:26:18 +0200

    specify and force vite port

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     plugins: [sveltekit()],
+    server: {
+        port: 5173,
+        strictPort: true,
+    },
     test: {
         include: ['src/**/*.{test,spec}.{js,ts}'],
     },
